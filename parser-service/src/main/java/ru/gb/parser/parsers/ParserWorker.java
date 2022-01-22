59b7465f992097208c509506f98eed997aa1f661
AndreyPomelov <k-udm@ya.ru>
2022-01-22 14:11:38 +0400

    #70 Добавить парсер событий с сайта skillfactory.ru

--- parser-service/src/main/java/ru/gb/parser/parsers/ParserWorker.java
+++ parser-service/src/main/java/ru/gb/parser/parsers/ParserWorker.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 public abstract class ParserWorker {
 
-    protected final String HREF_ATTR = "href";
+    protected static final String HREF_ATTR = "href";
 
     protected Document getPageByLink(String url) throws IOException {
        return Jsoup.connect(url)

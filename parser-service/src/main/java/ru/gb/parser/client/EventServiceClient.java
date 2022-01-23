fc2ed5314ca43e4a442aa96fbbe70c411aeb791c
DShcherbina <on@idimon.ru>
2022-01-24 00:36:49 +0300

    #82 docker
    Добавлены в docker-compose сервис парсера и бота
    Убран ненужный подмодуль front

--- parser-service/src/main/java/ru/gb/parser/client/EventServiceClient.java
+++ parser-service/src/main/java/ru/gb/parser/client/EventServiceClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import ru.gb.parser.domain.Event;
 
-@FeignClient(name = "EventServiceClient", url = "localhost:8081")
+@FeignClient(name = "EventService", url = "${event-service.url}")
 public interface EventServiceClient {
     @RequestMapping(method = RequestMethod.POST, value = "/events", consumes = MediaType.APPLICATION_JSON_VALUE)
     void saveEvent(@RequestBody Event event);

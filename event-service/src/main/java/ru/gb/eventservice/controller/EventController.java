fd6ac7a54e2627d8e798fd9e8244e14d8a7aac82
Dmitriy Shcherbina <on@idimon.ru>
2022-01-25 00:10:00 +0300

    Добавлен контролер и страница для ручного ввода событий

--- event-service/src/main/java/ru/gb/eventservice/controller/EventController.java
+++ event-service/src/main/java/ru/gb/eventservice/controller/EventController.java
@@ -2,6 +2,7 @@ package ru.gb.eventservice.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;

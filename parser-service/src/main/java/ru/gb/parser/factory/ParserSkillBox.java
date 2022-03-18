b6f2c1f64fad000cdc83a85319bc890dd502adf8
DShcherbina <on@idimon.ru>
2022-03-19 01:00:09 +0300

    add annotation

--- parser-service/src/main/java/ru/gb/parser/factory/ParserSkillBox.java
+++ parser-service/src/main/java/ru/gb/parser/factory/ParserSkillBox.java
@@ -1,8 +1,10 @@
 package ru.gb.parser.factory;
 
+import ru.gb.parser.annotation.SiteParser;
 import ru.gb.parser.parsers.ParserWorker;
 import ru.gb.parser.parsers.ParserWorkerSkillBox;
 
+@SiteParser
 public class ParserSkillBox extends Parser{
 
     @Override
--- parser-service/src/main/java/ru/gb/parser/factory/ParserSkillFactory.java
+++ parser-service/src/main/java/ru/gb/parser/factory/ParserSkillFactory.java
@@ -1,8 +1,10 @@
 package ru.gb.parser.factory;
 
+import ru.gb.parser.annotation.SiteParser;
 import ru.gb.parser.parsers.ParserWorker;
 import ru.gb.parser.parsers.ParserWorkerSkillFactory;
 
+@SiteParser
 public class ParserSkillFactory extends Parser {
 
     @Override
